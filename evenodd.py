aab1efee2b04dd72085cce7a79dd87f925b79539
Prashantism12 <pashu.s.madival@gmail.com>
2025-11-13 17:14:16 +0530

    Update evenodd.py

--- evenodd.py
+++ evenodd.py
@@ -1,12 +1,12 @@
 import sy
 numbers[13,14,5,8,9,16,30]
-even_count+=0
-odd_count+=0
+even_count+= 0
+odd_count+= 0
 for num in numbers:
-  if num % 2= 0
-  even_count+=1
-  else
-  odd_count+=1
+  if num % 2== 0:
+      even_count+= 1
+  else:
+       odd_coun += 1
 sys.stdout.write(f"count of even number:{even_count}\n")
 sys.stdout.write(f"count of odd number:{odd_count}\n")
 
